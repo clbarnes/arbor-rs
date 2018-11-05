0f04eb4d8be0e2854c4dbca92b4b6fd7e588b36f
Chris Barnes <barnesc@janelia.hhmi.org>
2018-11-05 15:18:45 -0500

    fix populate.sh

--- reference/populate.sh
+++ reference/populate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/catmaid/CATMAID/master/django/applications/catmaid/static/libs/catmaid/Arbor.js
-wget https://raw.githubusercontent.com/catmaid/CATMAID/master/django/applications/catmaid/static/libs/catmaid/arborparser.js
+wget https://raw.githubusercontent.com/catmaid/CATMAID/master/django/applications/catmaid/static/libs/catmaid/arbor_parser.js
 wget https://raw.githubusercontent.com/catmaid/CATMAID/master/django/applications/catmaid/static/libs/catmaid/synapse_clustering.js
